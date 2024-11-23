fc58256646ed0eced470c4dc80ade0fbd532c684
John Wang <johncwang@gmail.com>
2024-11-23 05:10:24 -0800

    enhance: `WriteFiles()`: add options for `writeDOCX`, `writePDF`

--- write.go
+++ write.go
@@ -7,11 +7,11 @@ import (
 	"github.com/grokify/mogo/os/fileext"
 )
 
-func WriteFilesLines(basename string, data []string, marginUnit string, marginScalar int, stdout io.Writer, stderr io.Writer) error {
-	return WriteFiles(basename, []byte(MarkdownLines(marginUnit, marginScalar, data)), stdout, stderr)
+func WriteFilesLines(basename string, data []string, marginUnit string, marginScalar int, writeDOCX, writePDF bool, stdout io.Writer, stderr io.Writer) error {
+	return WriteFiles(basename, []byte(MarkdownLines(marginUnit, marginScalar, data)), writeDOCX, writePDF, stdout, stderr)
 }
 
-func WriteFiles(basename string, data []byte, stdout io.Writer, stderr io.Writer) error {
+func WriteFiles(basename string, data []byte, writeDOCX, writePDF bool, stdout io.Writer, stderr io.Writer) error {
 	fileMkdn := basename + "." + fileext.ExtMarkdown
 	filePDF := basename + "." + fileext.ExtPDF
 	fileDOCX := basename + "." + fileext.ExtDOCX
@@ -20,23 +20,27 @@ func WriteFiles(basename string, data []byte, stdout io.Writer, stderr io.Writer
 		return err
 	}
 
-	popts := &PandocOpts{
-		InputFiles: []string{fileMkdn},
-		OutputFile: filePDF,
-		FromFormat: FormatMarkdown,
-		ToFormat:   FormatPDF,
-		Margin:     "",
+	if writePDF {
+		popts := &PandocOpts{
+			InputFiles: []string{fileMkdn},
+			OutputFile: filePDF,
+			FromFormat: FormatMarkdown,
+			ToFormat:   FormatPDF,
+			Margin:     "",
+		}
+		if err := Exec(popts, stdout, stderr); err != nil {
+			return err
+		}
 	}
-	if err := Exec(popts, stdout, stderr); err != nil {
-		return err
+	if writeDOCX {
+		popts2 := &PandocOpts{
+			InputFiles: []string{fileMkdn},
+			OutputFile: fileDOCX,
+			FromFormat: FormatMarkdown,
+			ToFormat:   FormatDOCX,
+			Margin:     "",
+		}
+		return Exec(popts2, stdout, stderr)
 	}
-
-	popts2 := &PandocOpts{
-		InputFiles: []string{fileMkdn},
-		OutputFile: fileDOCX,
-		FromFormat: FormatMarkdown,
-		ToFormat:   FormatDOCX,
-		Margin:     "",
-	}
-	return Exec(popts2, stdout, stderr)
+	return nil
 }
